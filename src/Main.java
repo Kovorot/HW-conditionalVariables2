e5929d8e59e7736043cdbb8a6b7447a39e9b3208
Kovorot <kovorot0875@mail.ru>
2024-06-10 20:22:04 +0300

    Methods added

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
+import java.time.LocalDate;
+
 public class Main {
 
     public static void main(String[] args) {
         //Задача 1
-        byte clientOS = 0;
+        int clientOS = 0;
 
         if (clientOS == 0) {
             System.out.println("Установите версию приложения для IOS по ссылке");
@@ -15,62 +17,16 @@ public class Main {
 
         //Задача 2
         clientOS = 1;
-        short clientDeviceYear = 2013;
-
-        if (clientOS == 0) {
-
-            if (clientDeviceYear < 2015) {
-                System.out.println("Установите облегченную версию приложения для IOS по ссылке");
-            } else {
-                System.out.println("Установите версию приложения для IOS по ссылке");
-            }
-        } else if (clientOS == 1) {
-
-            if (clientDeviceYear < 2015) {
-                System.out.println("Установите облегченную версию приложения для Android по ссылке");
-            } else {
-                System.out.println("Установите версию приложения для Android по ссылке");
-            }
-        } else {
-            System.out.println("К сожалению, приложение не поддерживает вашу операционную систему");
-        }
+        defineAppVersion(clientOS, LocalDate.now().getYear());
         System.out.println();
 
         //Задача 3
-        short year = 2001;
-        boolean result;
-
-        if (year % 4 == 0) {
-
-            if (year % 100 == 0) {
-                result = year % 400 == 0;
-            } else {
-                result = true;
-            }
-        } else {
-            result = false;
-        }
-
-        if (result) {
-            System.out.println(year + " год является високосным");
-        } else {
-            System.out.println(year + " год не является високосным");
-        }
+        isLeapYear(2027);
         System.out.println();
 
         //Задача 4
-        short deliveryDistance = 95;
-        byte deliveryTime;
-
-        if (deliveryDistance > 100) {
-            deliveryTime = -1;
-        } else if (deliveryDistance >  60) {
-            deliveryTime = 3;
-        } else if (deliveryDistance > 20) {
-            deliveryTime = 2;
-        } else {
-            deliveryTime = 1;
-        }
+        int deliveryDistance = 95;
+        int deliveryTime = calculateDeliveryTime(deliveryDistance);
 
         if (deliveryTime == -1) {
             System.out.println("Свыше 100 км нет доставки");
@@ -105,6 +61,73 @@ public class Main {
                 break;
             default:
                 System.out.println("Номер месяца может быть от 1 до 12");
+        }
+    }
+
+    /**
+     * Метод задачи 1.
+     * @param year год для проверки.
+     */
+    private static void isLeapYear(int year) {
+
+        if (year % 4 == 0) {
+
+            if (year % 100 == 0) {
+
+                if (year % 400 == 0) {
+                    System.out.println(year + " год - високосный год");
+                } else {
+                    System.out.println(year + " год невисокосный год");
+                }
+            } else {
+                System.out.println(year + " год - високосный год");
+            }
+        } else {
+            System.out.println(year + " год невисокосный год");
+        }
+    }
+
+    /**
+     * Метод задачи 2.
+     * @param operationSystem операционная система (0 - IOS, 1 - Android).
+     * @param deviceYear год выпуска устройства пользователя.
+     */
+    private static void defineAppVersion(int operationSystem, int deviceYear) {
+
+        if (operationSystem == 0) {
+
+            if (deviceYear < 2015) {
+                System.out.println("Установите облегченную версию приложения для IOS по ссылке");
+            } else {
+                System.out.println("Установите версию приложения для IOS по ссылке");
+            }
+        } else if (operationSystem == 1) {
+
+            if (deviceYear < 2015) {
+                System.out.println("Установите облегченную версию приложения для Android по ссылке");
+            } else {
+                System.out.println("Установите версию приложения для Android по ссылке");
+            }
+        } else {
+            System.out.println("К сожалению, приложение не поддерживает вашу операционную систему");
+        }
+    }
+
+    /**
+     * Метод задачи 3.
+     * @param deliveryDistance дистанция для доставки заказа.
+     * @return количество дней, необходимое для доставки.
+     */
+    private static int calculateDeliveryTime(int deliveryDistance) {
+
+        if (deliveryDistance > 100) {
+            return -1;
+        } else if (deliveryDistance >  60) {
+            return 3;
+        } else if (deliveryDistance > 20) {
+            return 2;
+        } else {
+            return 1;
         }
     }
 }
